5d66a76411fc70b2605ccf4cfb5498ffe82b5e97
Zac Orr <zac.orr@gmail.com>
2017-07-25 13:56:07 -0400

    First commit and push of python code

---
+++ learning.py
@@ -0,0 +1,13 @@
+def hello_world():
+    print('hello world')
+
+hello_world()
+print()
+
+
+colors = ['red', 'yellow', 'green']
+i = 0
+while i < len(colors):
+    print('this %d color is %s' % (i, colors[i]))
+    i = i + 1
+
